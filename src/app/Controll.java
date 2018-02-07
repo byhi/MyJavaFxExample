efa3555113f7a7b03fdbee0f6a0591999102a3d1
zoltan.bihari <zoltan.bihari@seizo-BZ-acer.seizo.hu>
2018-02-07 09:23:08 +0100

    Sec

--- src/app/Controll.java
+++ src/app/Controll.java
@@ -8,58 +8,58 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import model.Product;
-import test.App;
 
 public class Controll {
-	 @FXML private Button Gomb;
-	 @FXML private ListView<String> lista;
-	 @FXML private Button addGomb;
-	 
-	 @FXML private Button saveGomb;
-	 @FXML private  TextField ItemName;
-	 @FXML private  TextField ItemCost;
-	 @FXML private  TextField ItemQuantity; 
-	 @FXML private  TextField ItemType;
+	@FXML
+	private Button Gomb;
+	@FXML
+	private ListView<String> lista;
+	@FXML
+	private Button addGomb;
 
+	@FXML
+	private Button saveGomb;
+	@FXML
+	private TextField ItemName;
+	@FXML
+	private TextField ItemCost;
+	@FXML
+	private TextField ItemQuantity;
+	@FXML
+	private TextField ItemType;
 
-	 
-	 @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-		 loadEntitiesToList(controller.ProductController.getAllProductInList());					
-	    }
-	 
-	 @FXML protected void addButtonAction(ActionEvent event) {
-		// System.out.println("add something...");
-	    }
-	 
-	 
-	 
-	private void loadEntitiesToList(List<Product> list) {
-		 for (Product product : list) {
-			 lista.getItems().add(product.toString());
-		}	
+	@FXML
+	protected void handleSubmitButtonAction(ActionEvent event) {
+		loadEntitiesToList(controller.ProductController.getAllProductInList());
 	}
 
-	// private void //loadEntitiesToList(List<Product> allProductInList) {
-		// TODO Auto-generated method stub
-		
-	//}
+	@FXML
+	protected void addButtonAction(ActionEvent event) {
+		// System.out.println("add something...");
+	}
 
-	@FXML protected void saveData(ActionEvent event) {
-		 lista.getItems().add(new Person("Zoli", 20).toString());
-		 Product p = new Product(
-				 ItemName.getText(), 
-				 ItemCost.getText(), 
-				 ItemQuantity.getText(), 
-				 ItemType.getText()
-				 );
-		 System.out.println(p.toString());
-		 cleanFields();
-	    }
-	 
-	 private void cleanFields() {
-		 ItemName.setText("");
-		 ItemCost.setText(""); 
-		 ItemQuantity.setText(""); 
-		 ItemType.setText("");
-	}		
+	private void loadEntitiesToList(List<Product> list) {
+		for (Product product : list) {
+			lista.getItems().add(product.toString());
+		}
+	}
+
+	/*
+	 * private void //loadEntitiesToList(List<Product> allProductInList) {  }
+	 */
+
+	@FXML
+	protected void saveData(ActionEvent event) {
+		lista.getItems().add(new Person("Zoli", 20).toString());
+		Product p = new Product(ItemName.getText(), ItemCost.getText(), ItemQuantity.getText(), ItemType.getText());
+		System.out.println(p.toString());
+		cleanFields();
+	}
+
+	private void cleanFields() {
+		ItemName.setText("");
+		ItemCost.setText("");
+		ItemQuantity.setText("");
+		ItemType.setText("");
+	}
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class Main extends Application {
-	    
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -15,12 +15,12 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();						
-		} catch(Exception e) {
+			primaryStage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
